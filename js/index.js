84ac156ee87e05c9c8e792b834c9a4e3e290bcb3
rk <471091127@qq.com>
2017-10-11 17:29:29 +0800

    particle

--- js/index.js
+++ js/index.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
-    JoynBackground(1,4000); //运行（动画类型，时间）
-    //场景动画类型：1拉近 2拉远 3幻灯片显示 
+     new JParticles.particle('#particle',{
+     	resize:false
+     });
+    $("#nav-content").on("click",".to-content",function(e){
+    	var _this=$(this);
+    	_this.parent().addClass("active").siblings().removeClass("active")
+    	var top;
+    	if(this.hash==="#home"){
+    		top=0
+    	}else{
+    		top=$(this.hash).offset().top
+    	}	
+    	$("body,html").animate({
+    		scrollTop:top
+    	},600)
+    })
+    $(document).scroll(function(){
+    	if($(this).scrollTop()>300){
+    		$("#totop").show()
+    	}else{
+    		$("#totop").hide()
+    	}
+    })
+    $("#totop").click(function(){
+    	var _this=$(this)
+    	$("body,html").animate({
+    		scrollTop:0
+    	},600,function(){
+    		_this.hide()
+    	})
+    })
 })
