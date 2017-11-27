a6fc7b1887b1248e8ab7dbb8da6b18408e0132da
rk <471091127@qq.com>
2017-11-27 22:36:45 +0800

    note

--- js/index.js
+++ js/index.js
@@ -1,13 +1,72 @@
 $(document).ready(function(){
-     new JParticles.particle('#particle',{
-     	resize:false,
-     	range: 100,
-     })
     var isMobile = navigator.userAgent.match(/(iPhone|iPod|Android|BlackBerry|iPad|IEMobile|Opera Mini)/i), isIE9 = navigator.userAgent.match('MSIE 9.0');
     if(!isMobile){
     	$(".introduce-item").removeClass("animate");
     	scrollLoad()
     }
+    window.onload=actionBall
+    window.onresize=function(){
+    	clearInterval(timer)
+    	actionBall()
+    }
+    var timer=null,
+    	ballTime=20,
+    	ballNum=50,
+    	ballRadius=6,
+    	ballSpeed=6,
+    	colors=['rgb(70,222,222)', 'rgb(30,200,100)', 'rgb(238,20,130)', 'rgb(43,35,94)', 'rgb(255,200,100)', 'rgb(64,195,129)', 'rgb(5,11,250)', 'rgb(100,250,100)', 'rgb(0,255,255)', 'rgb(255,195,0)']; 	
+    function actionBall () {
+    	var mycanvas=document.getElementById("mycanvas");
+    	var wid =document.body.clientWidth;
+        var hei =document.body.clientHeight;
+	    balls=drawBall(mycanvas,wid,hei);
+	   	timer=setInterval(function() {
+	      animateBall(mycanvas,balls)
+	    }, ballTime)
+	  }
+      function createBall (w,h,colors,radius,speed) {
+        let obj = {}
+        obj.x = Math.random() * w
+        obj.y = Math.random() * h
+        obj.color = colors[Math.floor(Math.random() * colors.length)]
+        obj.radius = Math.floor(Math.random() * radius + radius)
+        obj.Vx = Math.floor(2*Math.random() * speed - speed) === 0 ? speed : Math.floor(2*Math.random() * speed - speed)
+        obj.Vy = Math.floor(2*Math.random() * speed - speed) === 0 ? speed : Math.floor(2*Math.random() * speed - speed)
+        return obj
+      }
+      function drawBall (el,w,h) {
+      	var balls=[];
+      	el.width=w;
+      	el.height=h;
+      	var ele = el.getContext('2d');
+        for (var i = 0; i < ballNum; i++) {
+          balls.push(createBall(w,h,colors,ballRadius,ballSpeed))
+        }       
+        ele.clearRect(0, 0, w, h)
+        for (var j = 0; j < balls.length; j++) {
+          ele.beginPath()
+          ele.fillStyle = balls[j].color
+          ele.arc(balls[j].x, balls[j].y, balls[j].radius, 0, 2 * Math.PI)
+          ele.fill()
+          ele.closePath()
+        }
+        return balls
+      }
+      function animateBall (el,balls) {
+        let ele = el.getContext('2d')
+        ele.clearRect(0, 0, el.width, el.height)
+        for (let i = 0; i < balls.length; i++) {
+          balls[i].x += balls[i].Vx
+          balls[i].y += balls[i].Vy
+          balls[i].x < 0 || balls[i].x >el.width ? balls[i].Vx = -balls[i].Vx : balls[i].Vx = balls[i].Vx
+          balls[i].y < 0 || balls[i].y >el.height ? balls[i].Vy = -balls[i].Vy : balls[i].Vy = balls[i].Vy
+          ele.beginPath()
+          ele.fillStyle = balls[i].color
+          ele.arc(balls[i].x + balls[i].Vx, balls[i].y + balls[i].Vy, balls[i].radius, 0, 2 * Math.PI)
+          ele.fill()
+          ele.closePath()
+        }
+      }
     function scrollLoad(){
     	var items=$(".introduce-item");
     	for(var i=0;i<items.length;i++){
